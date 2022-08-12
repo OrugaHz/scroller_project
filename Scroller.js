2c8c0743f6ad8c74ce56cd1bceddf3273071d4e5
Oruga <kanojodo@gmail.com>
2022-08-12 22:09:31 +0200

    Add browser compatibility

--- Scroller.js
+++ Scroller.js
@@ -27,7 +27,7 @@ class Scroller {
         return isVisible;
     }
 
-    listenScroll = (event) => {
+    listenScroll(event) {
         if (this.isThrottled) return;
         this.isThrottled = true;
 
@@ -35,12 +35,12 @@ class Scroller {
             this.isThrottled = false;
         }, 1000);
 
-        const direction = event.wheelDelta < 0 ? 1 : -1;
+        const direction = event.deltaY > 0 ? 1 : -1;
 
         this.scroll(direction);
 
     }
-    scroll = (direction) => {
+    scroll(direction) {
         if (direction === 1) {
             const isLastSection = this.currentSectionIndex === this.sections.length - 1;
             if (isLastSection) return;
@@ -54,7 +54,7 @@ class Scroller {
         this.scrollToCurrentSection();
     }
 
-    scrollToCurrentSection = () => {
+    scrollToCurrentSection() {
         this.selectActiveItem();
         this.sections[this.currentSectionIndex].scrollIntoView({
             behavior: 'smooth',
@@ -62,7 +62,7 @@ class Scroller {
         })
     }
 
-    drawNavigation = () => {
+    drawNavigation() {
         this.navigationContainer = document.createElement('aside');
         this.navigationContainer.setAttribute('class', 'scroller__navigation');
         const list = document.createElement('ul');
@@ -85,7 +85,7 @@ class Scroller {
         this.selectActiveItem()
     }
 
-    selectActiveItem = () => {
+    selectActiveItem() {
         if (this.navigationContainer) {
 
             const navigationItem = this.navigationContainer.querySelectorAll('li');
